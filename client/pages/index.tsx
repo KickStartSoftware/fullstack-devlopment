a7a509f2091771555bb2f51de04a17e4f52c2cbe
Taylor Head <81783723+Nero-DevOps@users.noreply.github.com>
2023-04-15 19:00:15 +0100

    Update index.tsx

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -7,6 +7,9 @@ import Core from '../components/general/Core';
 import Faq from '../components/general/Faq';
 import Repository from '../components/general/Repository';
 import PublicLayout from '../components/layout/public';
+import Image from "next/image";
+import laptop from "../assets/laptop3.png";
+
 
 export default function Home() {
   return (
