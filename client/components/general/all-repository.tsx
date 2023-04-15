aec3430df4e03f6fbd5f3fe65dc44eaf390b0141
Taylor Head <81783723+Nero-DevOps@users.noreply.github.com>
2023-04-15 18:59:46 +0100

    Update all-repository.tsx

--- client/components/general/all-repository.tsx
+++ client/components/general/all-repository.tsx
@@ -16,12 +16,12 @@ function Repository() {
       gutterTop="default"
       classname="bg-secondary-light px-4  mt-28 rounded-lg rounded-sm py-8 pt-12 md:px-8">
       <Typography variant="h1" text="Repository" textAlign="center" />
-      <Box classname="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4  2xl:grid-cols-6  py-12 mt-10 gap-x-6 gap-y-8">
+      <Box classname="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4  2xl:grid-cols-6  py-12 mt-10 gap-x-6 gap-y-8">
         {Object.keys(categorized).map(category => (
-          <Box key={category}>
-            <Typography variant="h5" text={category} gutterBottom="md" />
+          <Box key={category} classname='bg-[#040D18] rounded-2xl px-10 py-4 shadow'>
+            <Typography variant="h5" text={category} gutterBottom="md" classname='border-b-2 border-[#0ff6f6] pb-4 w-[170px] text-[#0ff6f6]' />
             {categorized[category].map((p: any) => (
-              <Box key={p._id} classname="flex items-center gap-x-4 mb-1">
+              <Box key={p._id} classname="flex items-center gap-x-4 mb-2">
                 <input
                   type="checkbox"
                   checked={isSelected(p)}
