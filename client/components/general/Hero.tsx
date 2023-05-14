025da9a11a8c0d8eb98d48cae2ae3ca9259afb2d
Taylor Head <81783723+Nero-DevOps@users.noreply.github.com>
2023-05-14 01:39:08 +0100

    update

--- client/components/general/Hero.tsx
+++ client/components/general/Hero.tsx
@@ -7,22 +7,26 @@ const Hero = () => {
     <div className="px-6 pt-8 md:pt-4 md:max-w-[95vw] lg:max-w-[85vw] mx-auto">
       <div className="flex flex-col md:flex-row justify-between  items-center  mx-auto">
         {/* Left Side */}
-        <div className="flex flex-col items-center  md:-mt-12">
-          <h1 className="text-[33px]   lg:text-[40px] xl:text-[50px] 2xl:text-[60px] font-bold text-white text-center md:whitespace-nowrap">
-            The key to <span className="text-[#0ff6f6]">downloading</span>
-            <br />
-            multiple software.
-          </h1>
-          <div className="flex items-center text-white text-[20px] lg:text-[25px] gap-4 pt-10">
-            <div className="bg-[#08eaea] px-6 py-1.5 font-semibold rounded-[0.5rem] hover:bg-[#26d3d3] transform duration-300 ease-in-out">
-              <button>Sign In</button>
-            </div>
-            <div className="bg-[#08eaea] px-4 py-1.5 font-semibold rounded-[0.5rem] hover:bg-[#26d3d3] transform duration-300 ease-in-out">
-              <button>Try Now</button>
-            </div>
-          </div>
-        </div>
-        {/* Right Side */}
+<div className="flex flex-col items-center  md:-mt-12">
+  <h1 className="text-[33px]   lg:text-[40px] xl:text-[50px] 2xl:text-[60px] font-bold text-white text-center md:whitespace-nowrap">
+    The key to <span className="text-[#0ff6f6]">downloading</span>
+    <br />
+    multiple software.
+  </h1>
+  <div className="flex items-center text-white text-[20px] lg:text-[25px] gap-4 pt-10">
+    <div className="bg-[#08eaea] px-6 py-1.5 font-semibold rounded-[0.5rem] hover:bg-[#26d3d3] transform duration-300 ease-in-out">
+      <a href="auth/sign-in">
+        <button>Sign In</button>
+      </a>
+    </div>
+    <div className="bg-[#08eaea] px-4 py-1.5 font-semibold rounded-[0.5rem] hover:bg-[#26d3d3] transform duration-300 ease-in-out">
+      <a href="auth/sign-up">
+        <button>Try Now</button>
+      </a>
+    </div>
+  </div>
+</div>
+{/* Right Side */}
         <div className="sm:mt-6">
           <Image
             alt=""
